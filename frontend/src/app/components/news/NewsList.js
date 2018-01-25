2e7fe18d431eb9a5651f68f2577fd173d78a1e10
Sergio <sergio.andres.ricardo@gmail.com>
2018-01-25 20:59:33 -0300

    backend folder

---
+++ frontend/src/app/components/news/NewsList.js
@@ -0,0 +1,11 @@
+class NewsListController {
+  constructor() {
+    this.text = 'My brand new componenasasdasdt!';
+  }
+}
+
+export const NewsList = {
+  template: require('./NewsList.html'),
+  controller: NewsListController
+};
+
