1b2e7c2b244728df50e7c20ca2a43eecf43ef573
sergio ricardo <sergio.ricardo@patagonian.it>
2018-02-04 03:31:23 -0300

    add extenxion and name to multimedia

--- radios-backend/grails-app/services/radios/backend/MultimediaService.groovy
+++ radios-backend/grails-app/services/radios/backend/MultimediaService.groovy
@@ -2,13 +2,17 @@ package radios.backend
 
 import Command.multimedia.SaveCommand
 import grails.gorm.services.Service
+import utils.FileUtils
 
 @Service(Multimedia)
 class MultimediaService {
     def cloudService
 
     Multimedia save(SaveCommand command) {
-        Multimedia multimedia = new Multimedia(type: command.type)
+        Multimedia multimedia = new Multimedia(
+                type: command.type,
+                extension: FileUtils.getExt(command.file),
+                name: command.file.originalFilename)
         cloudService.upload(command.file, multimedia)
         multimedia.save(failOnError: true)
         return multimedia
