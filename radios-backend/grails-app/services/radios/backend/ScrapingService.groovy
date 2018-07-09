12e14d671a977171c248a130fafbe8829d5e2135
JuanLucero <juan.ignacio.lucero@gmail.com>
2018-07-09 13:50:59 -0300

    scrap

--- radios-backend/grails-app/services/radios/backend/ScrapingService.groovy
+++ radios-backend/grails-app/services/radios/backend/ScrapingService.groovy
@@ -4,6 +4,7 @@ import Command.news.SaveCommand
 import grails.gorm.transactions.Transactional
 import org.jsoup.Jsoup
 
+
 @Transactional
 class ScrapingService {
     final String NEUQUEN_GOV = "http://w2.neuquen.gov.ar/actualidad/noticias"
@@ -11,28 +12,203 @@ class ScrapingService {
     NewsService newsService
 
     def scrap() {
+        News.findByTitle('Neuquén se sumó al plan nacional de Derechos Humanos')
         def doc = Jsoup.connect(NEUQUEN_GOV).userAgent("Mozilla/5.0").get()
         def articles = doc.select("a.readon")
         articles.each { article ->
             def raw_web = Jsoup.connect(NEUQUEN_GOV + article.attr("href")).get()
             def raw_article = raw_web.select("#ja-current-content")
             String raw = raw_article.toString().replaceAll('/images', NEUQUEN_GOV.substring(0, (NEUQUEN_GOV.length() - 20)) + '/images')
-            def a = newsService.save(
-                new SaveCommand([
-                    title         : raw_article.select('.contentheading span').text(),
-                    description   : raw_article.select('.article-content p b i').text(),
-                    rawDescription: raw,
-                    newsCategory  : NewsCategory.first(),
-                    radios        : [Radio.first()],
-                    user          : User.first(),
-                    image         : Multimedia.first().mediaId
-                ]))
+            def title = raw_article.select('.contentheading span').text()
+            if (News.findByTitle(title) == null) {
+                def a = newsService.save(
+                    new SaveCommand([
+                        title         : title,
+                        description   : raw_article.select('.article-content p b i').text(),
+                        rawDescription: raw,
+                        newsCategory  : NewsCategory.first(),
+                        radios        : [Radio.first()],
+                        user          : User.first(),
+                        image         : Multimedia.first().mediaId
+                    ]))
+            }
         }
-        def doc2 = Jsoup.connect(NEUQUEN_MUNI.substring(0,NEUQUEN_MUNI.length() -1 )).userAgent("Mozilla/5.0").get()
+        def doc2 = Jsoup.connect(NEUQUEN_MUNI).userAgent("Mozilla/5.0").get()
         def articles2 = doc2.select(".entry-title")
         articles2.each { article ->
-            def raw_web = Jsoup.connect( article.select('a').attr("href")).get()
-            println(raw_web)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            def raw_web = Jsoup.connect(article.select('a').attr("href")).get()
+            def raw_article = raw_web.select('.post')
+            def title = raw_article.select('.entry-title').text()
+            if (News.findByTitle(title) == null) {
+                def a = newsService.save(
+                    new SaveCommand([
+                        title         : title,
+                        description   : raw_article.select('p').not(".wp-caption-text")[0].text(),
+                        rawDescription: raw_article.select('p').text(),
+                        newsCategory  : NewsCategory.findByName(raw_article.attr("class").substring(raw_article.attr("class").lastIndexOf("category-") + 9).replaceAll('-', ' ')),
+                        radios        : [Radio.first()],
+                        user          : User.first(),
+                        image         : Multimedia.first().mediaId
+                    ])
+                )
+            }
         }
 
     }
